bd53e4913cd9fcee24ab841f9291c1cba26c0f5a
m21-cerutti <m21.cerutti@gmail.com>
2019-01-02 18:51:42 +0100

    Projet fonctionnel

---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    [SerializeField]
+    Rigidbody rb;
+
+    private Vector3 velocity;
+    private Vector3 rotation;
+    private float cameraRotationX;
+    private float currentCameraRotationX;
+    private float currentRotationY;
+    private float currentRotationZ;
+
+    [SerializeField]
+    private float cameraRotationLimit = 90f;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Utilisation de FixedUpdate pour toutes les updates liées à la physique
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(float _cameraRotationX)
+    {
+        cameraRotationX = _cameraRotationX;
+    }
+
+    private void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        currentCameraRotationX -= cameraRotationX;
+        currentRotationY += rotation.y;
+        currentRotationZ += rotation.z;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        cam.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+        transform.localEulerAngles = new Vector3(currentCameraRotationX, currentRotationY, currentRotationZ);
+    }
+}
---
+++ Assets/Scripts/IAScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IAScript : MonoBehaviour {
+
+    [Header("Attack")]
+    public Collider AreaOfHit;
+    public float HitCouldown;
+    public float HitDamage;
+
+    private GameObject m_target;
+    private NavMeshAgent m_agent;
+
+	// Use this for initialization
+	void Start () {
+        m_agent = GetComponent<NavMeshAgent>();
+		m_target = GameObject.Find("Platform");
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		//TODO hit if to close of target
+
+		//follow target
+		if(m_target != null)
+			m_agent.SetDestination(m_target.transform.position);
+    }
+
+}

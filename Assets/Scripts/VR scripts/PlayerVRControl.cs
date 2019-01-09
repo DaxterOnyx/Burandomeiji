9191de5361188362bddb16fc9dfeea19725a5f90
Daxter Onyx <daxter5onyx@gmail.com>
2019-01-09 23:52:00 +0100

    VR correct and start of Weapon gestion

---
+++ Assets/Scripts/VR scripts/PlayerVRControl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVRControl : MonoBehaviour {
+
+	List<GameObject> Weapons;
+	public GameObject ActiveWeapon	{ get;  private set;}
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	private void Awake()
+	{
+		Weapons = new List<GameObject>();
+		foreach (var item in gameObject.GetComponentsInChildren<WeaponScript>(true))
+		{
+			Weapons.Add(item.gameObject);
+			item.gameObject.SetActive(false);
+		}
+
+		ChangeActiveWeapon(Weapons[0]);
+	}
+
+	private void ChangeActiveWeapon(GameObject p_weapon)
+	{
+		if(gameObject == null)
+		{
+			UnityEngine.Debug.Log("new weapon is null");
+			return;
+		}
+		ActiveWeapon.SetActive(false);
+		ActiveWeapon.GetComponent<WeaponScript>().Grab(true);
+		ActiveWeapon = p_weapon;
+		ActiveWeapon.SetActive(true);
+		ActiveWeapon.GetComponent<WeaponScript>().Grab(false);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	
+}
---
+++ Assets/Scripts/VR scripts/WeaponScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	internal void Grab(bool v)
+	{
+		gameObject.GetComponent<VRTK.VRTK_InteractableObject>().Grabbed();
+	}
+}

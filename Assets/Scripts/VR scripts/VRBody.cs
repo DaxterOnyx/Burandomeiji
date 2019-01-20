3ecc7148bda27db5c7a29cf36042660eb44a3d65
Cerutti Marc <m21.cerutti@gmail.com>
2019-01-20 19:35:15 +0100

    Corp Vr de base fait

---
+++ Assets/Scripts/VR scripts/VRBody.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRBody : MonoBehaviour {
+
+    [SerializeField]
+    GameObject body;
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+        if(player != null)
+        {
+            Instantiate(body, player.transform, false);
+        }
+	}
+
+
+    private void Update()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                Instantiate(body, player.transform, false);
+            }
+        }
+    }
+}
